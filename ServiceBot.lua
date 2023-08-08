1448218e23e35f5f8955eddc76219a27c96d0fdd
Bobby <s215818@dtu.dk>
2023-08-09 01:37:44 +0200

    Added - Offense stats - Defense stats - Melee stats - Spell stats - Server reset

--- ServiceBot.lua
+++ ServiceBot.lua
@@ -32,7 +32,7 @@ Commands = {
     end,
 
 
-    -- Base Stats command
+    -- Base Stats: Strength, Agility, Stamina, Intellect, Spirit, Armor
     ["basestats"] = function(command, args)
         local strength = UnitStat("player", 1) or 0
         local agility = UnitStat("player", 2) or 0
@@ -42,6 +42,53 @@ Commands = {
         local armor = UnitArmor("player") or 0
         SendChatMessage("Base Stats: Strength: " .. strength .. "\n" .. "Agility: " .. agility .. "\n" .. "Stamina: " .. stamina .. "\n" .. "Intellect: " .. intellect .. "\n" .. "Spirit: " .. spirit .. "\n" .. "Armor: " .. armor, "GUILD")
     end,
+
+    -- Offense Stats: Damage, Attack Power, Attack Speed, DPS
+    ["offense"] = function(command, args)
+        local damage = GetSpellBonusDamage(2) or 0
+        local attackPower = UnitAttackPower("player") or 0
+        local attackSpeed = UnitAttackSpeed("player") or 0 
+        local dps = attackPower / attackSpeed
+        SendChatMessage("Offense stats: Damage: " .. damage .. "\n" .. "Attack Power: " .. attackPower .. "\n" .. "Attack Speed: " .. attackSpeed .. "\n" .. "DPS: " .. dps, "GUILD")
+    end,
+
+    -- Defense Stats: Armor, Block, Dodge, Parry, Resilience, Haste, Mastery
+    ["defense"] = function(command, args)
+        local armor = UnitArmor("player") or 0
+        local dodge = GetDodgeChance() or 0
+        local parry = GetParryChance() or 0
+        local block = GetBlockChance() or 0
+        local defense = GetCombatRating(2) or 0
+        SendChatMessage("Defense stats: Armor: " .. armor .. "\n" .. "Dodge: " .. dodge .. "\n" .. "Parry: " .. parry .. "\n" .. "Block: " .. block .. "\n" .. "Defense: " .. defense, "GUILD")
+    end,
+
+    -- Melee Enhancements Stats: Melee Hit, Melee +Damage, Melee Critical Strike, Ranged Critical Strike
+    ["melee"] = function(command, args)
+        local meleeHit = GetCombatRating(6) or 0
+        local meleeDamage = GetCombatRating(7) or 0
+        local meleeCrit = GetCombatRating(8) or 0
+        local rangedCrit = GetCombatRating(9) or 0
+        SendChatMessage("Melee Enhancement stats: Melee Hit: " .. meleeHit .. "\n" .. "Melee Damage: " .. meleeDamage .. "\n" .. "Melee Critical Strike: " .. meleeCrit .. "\n" .. "Ranged Critical Strike: " .. rangedCrit, "GUILD")
+    end,
+
+    -- Spell Enhancements Stats: Spell Hit, Spell +Damage, Spell Critical Strike, Spell Haste, +Healing, Mana Regen Not Casting, Mana Regen Casting
+    ["spell"] = function(command, args)
+        local spellHit = GetCombatRating(10) or 0
+        local spellDamage = GetCombatRating(11) or 0
+        local spellCrit = GetCombatRating(12) or 0
+        local spellHaste = GetCombatRating(13) or 0
+        local healing = GetCombatRating(14) or 0
+        local manaRegenNotCasting = GetCombatRating(15) or 0
+        local manaRegenCasting = GetCombatRating(16) or 0
+        SendChatMessage("Spell Enhancement stats: Spell Hit: " .. spellHit .. "\n" .. "Spell Damage: " .. spellDamage .. "\n" .. "Spell Critical Strike: " .. spellCrit .. "\n" .. "Spell Haste: " .. spellHaste .. "\n" .. "Healing: " .. healing .. "\n" .. "Mana regen (not casting): " .. manaRegenNotCasting .. "\n" .. "Mana regen (casting): " .. manaRegenCasting, "GUILD")
+    end,
+
+    -- print next server reset
+    ["reset"] = function(command, args)
+        local reset = GetQuestResetTime()
+        SendChatMessage("The next server reset is in: " .. reset .. " seconds.", "GUILD")
+    end,
+
 
     -- Separated stat commands for "separatedCmds" command
     -- ---------------------------------------------------
