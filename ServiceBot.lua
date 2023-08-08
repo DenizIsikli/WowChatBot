850ddf100cf77daae4516842fc942dc977b2f682
Bobby <s215818@dtu.dk>
2023-08-08 17:10:54 +0200

    Added more commands

--- ServiceBot.lua
+++ ServiceBot.lua
@@ -9,24 +9,41 @@ frame:SetScript("OnEvent", function(self, event, message)
         
         if command then
             commandList(command, args)
+            main()
         end
     end
 end)
 
 Commands = {
     -- List of all available commands
+        -- Predominantly random commands for all purposes of gathering information
 
+    -- Help command
     ["help"] = function(command, args)
         SendChatMessage("Available commands: !help, !crit", "GUILD")
     end,
+
+    -- Crit command
     ["crit"] = function(command, args)
         local crit = GetCritChance()
         SendChatMessage("Your current crit chance is: " .. crit .. "%")
     end,
+
+    -- Attack power command
+    ["ap"] = function(command, args)
+        local ap = GetAttackPower()
+        SendChatMessage("Your current attack power is: " .. ap .. " AP")
+    end,
+
+    -- Spell power command
+    ["sp"] = function(command, args)
+        local sp = GetSpellBonusDamage(2)
+        SendChatMessage("Your current spell power is: " .. sp .. " SP")
+    end,
 }
 
-
-
+-- Command List function that executes the specified command 
+-- depending on the input from the user
 function commandList(command, args)
     local commandFunction = Commands[command]
     
@@ -38,5 +55,8 @@ function commandList(command, args)
 end
 
 
+-- Main function
+function main ()
+    commandList()
+end
 
-    
